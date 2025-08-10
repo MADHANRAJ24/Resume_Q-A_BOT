884bee712a87b9d90a822fa78d26f68b961ecf97
MADHANRAJ24 <mathan2412005@gmail.com>
2025-08-10 14:05:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import re
 load_dotenv(override=True)
 
 # Initialize Groq LLM
-llm = ChatGroq(model="mixtral-8x7b-32768", temperature=0.7)
+llm = ChatGroq(model="llama-3.3-70b-versatile", temperature=0.7)
 
 st.set_page_config(page_title="Resume Q&A BOT")
 st.title("Resume Based Interview Q&A Bot")
@@ -75,4 +75,5 @@ if upload_file is not None:
 
         st.subheader("Generated Q&A")
         st.markdown(response.content)
+
 
